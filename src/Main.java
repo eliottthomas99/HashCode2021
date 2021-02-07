37cfc27d70bb33fc1001ee976485d92ef5d794f1
ju <jthomas@enssat.fr>
2021-02-07 14:26:49 +0100

    Squelette v 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        // recuperer les donnees
+        // objets ?
+        // - nb pizzas
+        // - nb equipes de 2
+        // - nb equipes de 3
+        // - nb equipes de 4
+        // - liste de pizzas
+                // pizzas :
+                //          - nb ingredients
+                //          - liste ingredients
+
+
+
+        // traitement
+
+
+
+
+
+
+
+        // renvoyer les donnees
+
+    }
 }
