a7b476a64916e2a7ec22c156701e5267f571ddfe
Eliott <ethomas@enssat.fr>
2021-02-25 19:34:38 +0100

    temps feu vert

--- src/Rue.java
+++ src/Rue.java
@@ -1,4 +1,29 @@
 public class Rue {
     public String nom;
     public int T;
+    public int tempsFeuVert;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getT() {
+        return T;
+    }
+
+    public void setT(int t) {
+        T = t;
+    }
+
+    public int getTempsFeuVert() {
+        return tempsFeuVert;
+    }
+
+    public void setTempsFeuVert(int tempsFeuVert) {
+        this.tempsFeuVert = tempsFeuVert;
+    }
 }
